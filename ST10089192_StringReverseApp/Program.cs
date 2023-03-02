fb30294fabdd487680b81ef975af4b9b5039a6f4
Katiso <ST10089192@vcconnect.edu.za>
2023-03-02 12:45:46 +0000

    First commit

--- ST10089192_StringReverseApp/Program.cs
+++ ST10089192_StringReverseApp/Program.cs
@@ -6,17 +6,17 @@
         {
 
             Console.WriteLine("Please type a word: ");
-            string input = Console.ReadLine();
+            string input = Console.ReadLine();//recieve user input
             string output = null; 
 
-            for(int i = input.Length - 1; i >= 0; i--)
+            for(int i = input.Length - 1; i >= 0; i--)//for loop to iterate backwords through the string
             {
                 output += input[i];
             }
 
-            Console.WriteLine("Watch the magic...");
+            Console.WriteLine("Watch the magic..." 
+                + "\nYour word in reverse is: " + output);//print output
 
-            Console.WriteLine("\nHere it is in reverse: " + output);
             Console.ReadLine();
         }
     }
