41efa637c9b52eee0b3213ffc3541c076a9daff2
Katiso <ST10089192@vcconnect.edu.za>
2023-03-02 11:11:03 +0000

    First commit

---
+++ ST10089192_StringReverseApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ST10089192_StringReverseApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please type a word: ");
+            string input = Console.ReadLine();
+            string output = null; 
+
+            for(int i = input.Length - 1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+
+            Console.WriteLine("Watch the magic...");
+
+            Console.WriteLine("\nHere it is in reverse: " + output);
+            Console.ReadLine();
+        }
+    }
+}
